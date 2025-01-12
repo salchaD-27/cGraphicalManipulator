46f32ecb3bbfa6fbd2b95b107672b1277bd4914e
Dharyansh Achlas <164983962+salchaD-27@users.noreply.github.com>
2025-01-13 01:13:11 +0530

    Add files via upload

---
+++ build/CMakeFiles/cGraphicalManipulator.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cGraphicalManipulator.dir/src/cGraphicalManipulator.c.o"
+  "CMakeFiles/cGraphicalManipulator.dir/src/cGraphicalManipulator.c.o.d"
+  "CMakeFiles/cGraphicalManipulator.dir/src/stb_image.c.o"
+  "CMakeFiles/cGraphicalManipulator.dir/src/stb_image.c.o.d"
+  "CMakeFiles/cGraphicalManipulator.dir/src/stb_image_write.c.o"
+  "CMakeFiles/cGraphicalManipulator.dir/src/stb_image_write.c.o.d"
+  "cGraphicalManipulator"
+  "cGraphicalManipulator.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/cGraphicalManipulator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/cGraphicalManipulator.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for cGraphicalManipulator.
+# This may be replaced when dependencies are built.
